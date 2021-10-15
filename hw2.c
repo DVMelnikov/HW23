c235fd1240203134fa64b066023e138f7ccf04ba
DVMelnikov <mit23560@gmail.com>
2021-10-15 04:18:10 +0300

    dz2 -completed(draft)

--- hw2.c
+++ hw2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define STR 3
-#define ROW 3
+#define COLUMN_FINAL_ARR 2
+
 
 /*Проверки: 
     Удалось ли открыть файл
@@ -29,8 +30,8 @@ int main(void) {
 
     rewind(text);
 
-    int row = 1;
     int str = 0;
+    int column = 1;
 
     while (!feof(text)) // строки
          if (fgetc(text) == '\n')
@@ -39,37 +40,91 @@ int main(void) {
     rewind(text);
 
     while (fgetc(text) != '\n')  //столбцы
-        row++;
-
-    printf("row = %d str = %d\n", row, str );
+        column++;
+/*
+    printf("str = %d column = %d\n", str, column ); */
 
     int **mat_inc;
-    mat_inc = (int**)calloc(row, sizeof(int*));
-    for (int i = 0; i < row; i++) {
-        mat_inc[i] = (int*)calloc(str, sizeof(int));
+    mat_inc = (int**)calloc(str, sizeof(int*));
+    for (int i = 0; i < str; i++) {
+        mat_inc[i] = (int*)calloc(column, sizeof(int));
     }
 
     rewind(text);
 
-    for (int i = 0; i < row; i++) { // ввод в масисив
-        for (int j = 0; j < str; j++) {
-            mat_inc[i][j] = fgetc(text);
+    for (int i = 0; i < str; i++) { // ввод в масисив
+        for (int j = 0; j < column; j++) {
+            if (!feof(text))
+                mat_inc[i][j] = fgetc(text);
         }
     }
-
-    for (int i = 0; i < row; i++) { // вывод массива
-        for (int j = 0; j < str; j++) {
-            printf("%c", (char)mat_inc[i][j]);
-        }
-    }
-
     fclose(text);
-
     
-    for (int i = 0; i < row; i++) {
+    int flag = 0;
+
+    int **final_arr;
+    int str_final_arr = (column / 2) + 1;
+    final_arr = (int**)calloc(str_final_arr, sizeof(int*));
+    for (int i = 0; i < str_final_arr; i++) {
+        final_arr[i] = (int*)calloc(COLUMN_FINAL_ARR, sizeof(int));
+    }
+/*
+    for (int i = 1; i < str; i++) {
+        for (int j = 2; j < column; j++) {
+            printf("str = %d column = %d: %c\n", i, j, mat_inc[i][j]);
+            // printf("%c", (char)mat_inc[i][j]);
+        }
+    } */
+    int count_str_final_arr = 0;
+
+    for (int i = 2; i < column; i++) {
+        if (!isdigit(mat_inc[1][i])) {
+            continue;
+        }
+        for (int j = 1; j < str; j++) {
+            if (mat_inc[j][i] == '1') {
+                final_arr[count_str_final_arr][flag] = j;
+                flag++;
+            }
+        }
+        if (flag == 1) { // петля
+            final_arr[count_str_final_arr][flag] = final_arr[count_str_final_arr][flag - 1];
+        }
+        flag = 0;
+        count_str_final_arr++;
+    }
+
+    for (int i = 0; i < count_str_final_arr; i++ ) {
+        for (int j = 0; j < COLUMN_FINAL_ARR; j++) {
+            printf("%d ", final_arr[i][j]);
+        }
+        printf("\n"); 
+    }
+    
+    FILE * for_dot = fopen("out.gv", "w+");
+
+    fprintf(for_dot, "graph NEW {\n");
+
+    for (int i = 0; i < count_str_final_arr; i++) {
+            fprintf(for_dot, "\t%d -- %d\n", final_arr[i][0], final_arr[i][1]);
+    }
+
+    fprintf(for_dot, "}");
+
+    fclose(for_dot);
+
+    system("dot -Tpng out.gv -o out.jpg");
+    system("wslview out.jpg");
+
+    for (int i = 0; i < str; i++) {
         free(mat_inc[i]);
     }
     free(mat_inc);
+
+    for (int i = 0; i < str_final_arr; i++) {
+        free(final_arr[i]);
+    }
+    free(final_arr);
 
     return 0;
 }
